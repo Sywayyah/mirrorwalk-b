0eb5764a0ca82f9ad676619bf79b129984fcfb81
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-06-19 03:50:13 +0500

    notes about icons

---
+++ src/app/notes/ui.ts
@@ -0,0 +1,9 @@
+/* 
+  now I have rpg awesome. I can also use it for special effects.
+  
+  icon (color, size)
+  icon (color, blur, mix-blend-mode) (remember: overlay works when it's dark below)
+
+  also there could be some special effects like expanding lightened round 
+  or sparks, or some spinning element or elements...
+*/
