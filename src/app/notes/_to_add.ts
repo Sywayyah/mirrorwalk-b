7898680d338441a07b4f8aef276b2cc291ff3d94
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-07-11 22:38:47 +0500

    _to_add: notes of some things to add

---
+++ src/app/notes/_to_add.ts
@@ -0,0 +1,14 @@
+/* 
+    Ideas of things to add
+
+    Add: item which grants move speed to random unit group at the beginnnig
+      of the fight. This would help with making the first move.
+
+    Add: item which allows to cast Enchant-like spell, so casters can increase
+      their damage output within the same round.
+    
+    Add: item which allows to instantly cast Enchant agains all enemies.
+
+    Idea: Dueling. Dueling can reduce enemy's damage when he is attacking other than
+      someone he's dueling now.
+*/
