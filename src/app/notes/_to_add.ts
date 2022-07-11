af791e163004002380f6af757f1b084768a6ab06
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-07-11 22:42:09 +0500

    _to_add: special masteries, unique ways to learn them

--- src/app/notes/_to_add.ts
+++ src/app/notes/_to_add.ts
@@ -1,5 +1,5 @@
 /* 
-    Ideas of things to add
+    Short ideas of things to add
 
     Add: item which grants move speed to random unit group at the beginnnig
       of the fight. This would help with making the first move.
@@ -11,4 +11,11 @@
 
     Idea: Dueling. Dueling can reduce enemy's damage when he is attacking other than
       someone he's dueling now.
+
+    Idea: There could be some ways to not only learn abilities, but also masteries
+      at the same time. For example, visiting Witch Hut can grant a hero both 
+      Enchant and some Mastery, that allows to cast is instantly against all enemies.
+
+      And it also could be the only way to achieve this Mastery, would be no other ways,
+      only Witch Hut.
 */
