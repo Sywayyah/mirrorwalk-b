343d873573aa934c3e4571f9cfd9ab067de3c93d
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-07-11 22:12:16 +0500

    mechanics notes

---
+++ src/app/notes/_mechanics.ts
@@ -0,0 +1,19 @@
+/* 
+    Here are just some mechanics. Some may be used, some are just described, 
+        some can be potential.
+
+    (Potential) Crests. Crest is a special item. Crest may give a player some
+        helpful abilities, for example:
+        
+        Rangers Guild Crest: you start each round with 8 Rangers;
+        
+        Arcane Crest: Allows to use basic spells with cooldown of 1 enemy turn;
+
+        Portents Crest: Allows to use basic curses with cooldown of 1 enemy turn;
+
+        Ice Crest: Reduces all enemy groups move speed by 3;
+
+    
+    (Used) Heroes may start with certain items. This can make some interesting heroes.
+    
+*/
