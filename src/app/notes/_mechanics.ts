1fe43a3e3fba533430415329f18dec63a9f36e51
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-07-11 22:22:04 +0500

    _mechanics: Spells and Masteries

--- src/app/notes/_mechanics.ts
+++ src/app/notes/_mechanics.ts
@@ -13,7 +13,20 @@
 
         Ice Crest: Reduces all enemy groups move speed by 3;
 
+        Crests can be sold and bought in town structures or neutral ones. It could be
+        that player can have more than one crest, but some of them may not stack.
     
     (Used) Heroes may start with certain items. This can make some interesting heroes.
     
+    (Planned) Heroes, on level up, can upgrade Spells or Specialties. There are some spells
+        which are developed well, and some of them also heavily rely on Masteries.
+
+        For example, Rain of Fire, when leveled, can target additional random enemy group,
+        and Meteor, with a certain Mastery, can land instantly, instead of waiting one turn.
+
+        Also.. can it be that there are some special properties in spells coming with
+        next levels (or not coming, some are unobtainable from leveling spells),
+        and some Masteries can just speed up this process? For example, on the level 4,
+        Rain of Fire adds one more random target, and what if there would be a Mastery which
+        allows to have this effect earlier?
 */
