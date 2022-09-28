b3e8aa7ced8a0cfbce508bd2a9450ef299e7cbe0
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-09-29 00:12:46 +0500

    Remove unused duplicated utils

--- src/app/feature-sandbox/services/mw-battle-events.service.ts
+++ src/app/feature-sandbox/services/mw-battle-events.service.ts
@@ -1,19 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
 import { EventsServiceBase } from './state/events-service-base';
 import { BattleEvent, EventByEnumMapping } from './types';
-
-
-function isNullish<T>(value: T | null | undefined): value is T {
-  return value !== null && value !== undefined ? true : false;
-}
-
-function filterNullish<T>(source$: Observable<T | null | undefined>): Observable<T> {
-  return source$.pipe(
-    filter(isNullish),
-  );
-}
 
 @Injectable({
   providedIn: 'root'
