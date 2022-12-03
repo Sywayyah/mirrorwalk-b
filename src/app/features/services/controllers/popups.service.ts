e0727269fb0fb23d874502e0961d2914771563c6
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-12-03 21:27:27 +0500

    Move displaying popup on struct selected from views component to popups controller

--- src/app/features/services/controllers/popups.service.ts
+++ src/app/features/services/controllers/popups.service.ts
@@ -1,10 +1,10 @@
 import { Injectable, Type } from '@angular/core';
-import { NeutralCampStructure, NeutralRewardTypesEnum } from 'src/app/core/structures';
-import { FightEndsPopup, LossModel } from 'src/app/core/ui';
+import { NeutralCampStructure, NeutralRewardTypesEnum, NeutralSite, StructureTypeEnum } from 'src/app/core/structures';
+import { FightEndsPopup, LossModel, PrefightPopup, PreviewPopup, UpgradingPopup } from 'src/app/core/ui';
 import { StoreClient, WireMethod } from 'src/app/store';
-import { HiringRewardPopupComponent, ItemRewardPopupComponent, PostFightRewardPopupComponent, ResourcesRewardPopupComponent, ScriptedRewardPopupComponent } from '../../battleground/components';
+import { HiringRewardPopupComponent, ItemRewardPopupComponent, PostFightRewardPopupComponent, PreFightPopupComponent, PreviewPopupComponent, ResourcesRewardPopupComponent, ScriptedRewardPopupComponent, UpgradeRewardPopup } from '../../battleground/components';
 import { PopupData, PopupService } from '../../shared/components';
-import { DisplayPopup, DisplayReward, FightEnds, FightEndsEvent, NeutralStructParams } from '../events';
+import { DisplayPopup, DisplayReward, FightEnds, FightEndsEvent, NeutralStructParams, StructSelected, StructSelectedEvent } from '../events';
 import { BattleStateService } from '../mw-battle-state.service';
 import { MwPlayersService } from '../mw-players.service';
 
@@ -24,6 +24,48 @@ export class PopupsController extends StoreClient() {
   public displayPopup(event: PopupData): void {
     console.log('does it happen?')
     this.popupService.createPopup(event);
+  }
+
+
+  @WireMethod(StructSelected)
+  public playerSelectsStructure(event: StructSelectedEvent): void {
+    if (event.struct.type === StructureTypeEnum.NeutralCamp) {
+      const prefightPopup: PrefightPopup = {
+        struct: event.struct as NeutralCampStructure,
+      };
+
+
+      this.events.dispatch(DisplayPopup({
+        component: PreFightPopupComponent,
+        popup: prefightPopup
+      }));
+      return;
+    }
+
+    if (event.struct.type === StructureTypeEnum.NeutralSite) {
+      if (event.struct.generator.onVisited) {
+        const previewPopup: PreviewPopup = {
+          struct: event.struct as NeutralSite,
+        };
+
+        this.events.dispatch(DisplayPopup({
+          component: PreviewPopupComponent,
+          popup: previewPopup
+        }));
+
+        return;
+      }
+
+      const upgradingPopup: UpgradingPopup = {
+        struct: event.struct as NeutralSite,
+      };
+
+      this.events.dispatch(DisplayPopup({
+        component: UpgradeRewardPopup,
+        popup: upgradingPopup,
+      }));
+
+    }
   }
 
   @WireMethod(FightEnds)
--- src/app/features/views/components/mw-view-control/mw-view-control.component.ts
+++ src/app/features/views/components/mw-view-control/mw-view-control.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
-import { NeutralCampStructure, NeutralSite, StructureTypeEnum } from 'src/app/core/structures';
-import { PrefightPopup, PreviewPopup, UpgradingPopup } from 'src/app/core/ui';
-import { PreFightPopupComponent, PreviewPopupComponent, UpgradeRewardPopup } from 'src/app/features/battleground/components';
-import { DisplayPopup, DisplayReward, NeutralStructParams, StructCompleted, StructFightConfirmed, StructSelected, StructSelectedEvent } from 'src/app/features/services/events';
+import { DisplayReward, NeutralStructParams, StructCompleted, StructFightConfirmed } from 'src/app/features/services/events';
 import { Notify, StoreClient, WireMethod } from 'src/app/store';
 
 enum ViewsEnum {
@@ -18,47 +15,6 @@ enum ViewsEnum {
 export class MwViewControlComponent extends StoreClient() {
   public currentView: ViewsEnum = ViewsEnum.Structures;
   public viewTypes: typeof ViewsEnum = ViewsEnum;
-
-  @WireMethod(StructSelected)
-  public playerSelectsStructure(event: StructSelectedEvent): void {
-    if (event.struct.type === StructureTypeEnum.NeutralCamp) {
-      const prefightPopup: PrefightPopup = {
-        struct: event.struct as NeutralCampStructure,
-      };
-
-
-      this.events.dispatch(DisplayPopup({
-        component: PreFightPopupComponent,
-        popup: prefightPopup
-      }));
-      return;
-    }
-
-    if (event.struct.type === StructureTypeEnum.NeutralSite) {
-      if (event.struct.generator.onVisited) {
-        const previewPopup: PreviewPopup = {
-          struct: event.struct as NeutralSite,
-        };
-
-        this.events.dispatch(DisplayPopup({
-          component: PreviewPopupComponent,
-          popup: previewPopup
-        }));
-
-        return;
-      }
-
-      const upgradingPopup: UpgradingPopup = {
-        struct: event.struct as NeutralSite,
-      };
-
-      this.events.dispatch(DisplayPopup({
-        component: UpgradeRewardPopup,
-        popup: upgradingPopup,
-      }));
-
-    }
-  }
 
   @Notify(StructFightConfirmed)
   public playerAcceptsFight(): void {
