931152bb3c8d517d5de005f62b0fa3946852a018
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-12-06 23:35:10 +0500

    adjust indentation

--- src/app/core/fractions/humans/fraction.ts
+++ src/app/core/fractions/humans/fraction.ts
@@ -1,10 +1,10 @@
 import { Fractions, FractionsEnum } from '../fractions';
 
 export type HUMANS_UNIT_TYPES = 'Archer'
-| 'Pikeman'
-| 'Knight'
-| 'Cavalry'
-| 'Firebird';
+  | 'Pikeman'
+  | 'Knight'
+  | 'Cavalry'
+  | 'Firebird';
 
 export const humansFraction = Fractions.createFraction<HUMANS_UNIT_TYPES>(FractionsEnum.Humans, {
   title: 'Castle',
