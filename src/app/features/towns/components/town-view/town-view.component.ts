8af80ce0319d8cabaccb867f68191b8b7bc82bb0
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-12-14 22:12:48 +0500

    Small basic changes to towns

--- src/app/features/towns/components/town-view/town-view.component.ts
+++ src/app/features/towns/components/town-view/town-view.component.ts
@@ -19,20 +19,20 @@ export class TownViewComponent {
     private events: EventsService,
   ) {
     this.buildingsByTiers = Object.entries(this.town.base.availableBuildings).reduce((map, [id, building]) => {
-      // console.log(map);
-      // console.log({ ...map }, building.tier, 'is there no such tier?', !!{ ...map }[building.tier]);
       if (!map[building.tier]) {
-        // console.log(building.tier, 'how many times do I enter this?');
         map[building.tier] = [this.town.buildings[id]];
       } else {
         map[building.tier].push(this.town.buildings[id]);
       }
       return map;
     }, {} as Record<number, Building[]>);
-    console.log('--------> ', this.buildingsByTiers);
   }
 
   public leaveTown(): void {
     this.events.dispatch(PlayerLeavesTown({}));
+  }
+
+  public handleBuildingClicked(building: Building): void {
+    console.log('building: ', building);
   }
 }
