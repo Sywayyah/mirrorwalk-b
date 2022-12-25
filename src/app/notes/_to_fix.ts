31e388602a1f846b963581ba7c733c35d2078121
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2022-12-25 20:27:29 +0500

    Some comments

--- src/app/notes/_to_fix.ts
+++ src/app/notes/_to_fix.ts
@@ -1,4 +1,6 @@
 /*
-  Spells interactions, if I press targeted spell without casting it, then will press instant spell,
+  - Spells interactions, if I press targeted spell without casting it, then will press instant spell,
   game will still remember targeting state.
+
+  - Healing and tail hp mechanics.
 */
