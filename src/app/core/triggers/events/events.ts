ba736ebad7754968300eb04fb12fe978bf114ec1
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2023-03-06 17:52:36 +0500

    Rename trigger events

--- src/app/core/triggers/events/events.ts
+++ src/app/core/triggers/events/events.ts
@@ -2,12 +2,12 @@ import { eventsForPrefix } from 'src/app/store';
 import { DefaultGameModes } from '../types';
 import { GamePreparedEvent } from './types';
 
-const createCustomEvent = eventsForPrefix('[Custom]');
+const createTriggerEvent = eventsForPrefix('[Triggers]');
 
-export const PrepareGameEvent = createCustomEvent<{ gameMode: DefaultGameModes }>(
+export const PrepareGameEvent = createTriggerEvent<{ gameMode: DefaultGameModes }>(
   'Player started new game, waiting for GamePreparationFinished event to prepare the map.',
 );
 
-export const GamePreparationFinished = createCustomEvent<GamePreparedEvent>(
+export const GamePreparationFinished = createTriggerEvent<GamePreparedEvent>(
   'Map is prepared, player can start the game',
 );
