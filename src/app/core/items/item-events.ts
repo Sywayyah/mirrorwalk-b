7c2dba314d138c09c6c895a63a68b8019f655db0
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2023-04-03 22:03:16 +0500

    Minor code prettifying

--- src/app/core/items/item-events.ts
+++ src/app/core/items/item-events.ts
@@ -1,12 +1,6 @@
 import { createEventType } from 'src/app/store';
 import { EventHandlersMap, EventNames, createEventsGroup } from 'src/app/store/events/event-groups';
 
-
-// it feels like hybrid approach can be used.
-// events object above can be used for easy use
-// and refactoring, while events group can
-// prepare events for logging, give ability to
-// get item event by name, etc.
 export const ItemsEventsGroup = createEventsGroup({
   prefix: 'Items',
   events: {
@@ -14,8 +8,8 @@ export const ItemsEventsGroup = createEventsGroup({
   },
 });
 
-
 export const ItemEvents = ItemsEventsGroup.events;
+
 
 export type ItemGroupType = typeof ItemsEventsGroup;
 
--- src/app/core/spells/spell-events.ts
+++ src/app/core/spells/spell-events.ts
@@ -27,24 +27,3 @@ export type SpellGroupType = typeof SpellEventsGroup;
 export type SpellEventNames = EventNames<SpellGroupType>;
 export type SpellEventTypeByName<K extends SpellEventNames> = EventTypeByName<SpellGroupType, K>;
 export type SpellEventHandlers = EventHandlersMap<SpellGroupType>;
-
-
-// batch creation? grouped events? is it going to be useful? Gonna have to consider a couple of things
-// at very least.
-
-// gonna need to think how places in code are going to look
-//  it feels like all events are going to be used with prefix of the group.
-//  although, there can be a helper type straight from store folder, and it can actually
-//  work to use strings/enums, because here we are going to store both strings/Events regardless
-
-//  so, there will be 2 ways to work with groupedEvents, by group and string keys of this group
-//   strings look the simplest, and gonna need to see if they are going to be refactorable,
-//   although I'm not very sure about strings in terms of memory usage, since
-//   there is no string pool in js
-
-// There is at least one advantage: if I'm ever going to implement logging, by creating grouped events
-//   I will be able to reuse their key in logs, this way descriptions won't be so crucial, and
-//    can play a descriptive role even per case.
-
-// anyways, I think I need to give it a shot. It is going to take some time to implement
-//  and think through, and might be even discarded in the end (but looks interesting and promising)
