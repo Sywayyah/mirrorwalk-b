6e04faef7ec9d0005ea037dad15632f668a27dd3
Yauheni Zharykau <fakeemail@gmail.com>
2023-05-07 20:10:11 +0500

    Provide groups for events

--- src/app/core/events/triggers/events.ts
+++ src/app/core/events/triggers/events.ts
@@ -1,23 +1,8 @@
-import { createEventsGroup, eventsForPrefix } from 'src/app/store';
+import { eventsForPrefix } from 'src/app/store';
 import { DefaultGameModes, GamePreparedEvent } from './types';
 
 const createTriggerEvent = eventsForPrefix('[Triggers]');
 
-export const TriggersEventGroup = createEventsGroup({
-  prefix: 'Triggers',
-  events: {
-    PrepareGameEvent: createTriggerEvent<{ gameMode: DefaultGameModes }>(
-      'Player started new game, waiting for GamePreparationFinished event to prepare the map.',
-    ),
-    GamePreparationFinished: createTriggerEvent<GamePreparedEvent>(
-      'Map is prepared, player can start the game',
-    ),
-  },
-});
-
-export const Triggers = TriggersEventGroup.events;
-
-/*
 export const PrepareGameEvent = createTriggerEvent<{ gameMode: DefaultGameModes }>(
   'Player started new game, waiting for GamePreparationFinished event to prepare the map.',
 );
@@ -25,4 +10,3 @@ export const PrepareGameEvent = createTriggerEvent<{ gameMode: DefaultGameModes 
 export const GamePreparationFinished = createTriggerEvent<GamePreparedEvent>(
   'Map is prepared, player can start the game',
 );
- */
