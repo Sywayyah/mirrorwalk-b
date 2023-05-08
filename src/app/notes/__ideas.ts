2133ef3bc5fa66f2a039edee1ad6b5c90bc44b38
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2023-05-08 20:06:03 +0500

    Some ideas

---
+++ src/app/notes/__ideas.ts
@@ -0,0 +1,19 @@
+/*
+  Fire-themed ideas:
+    Firestorm: Deals 40 fire damage +20-40 additional damage,
+      but also deals the lower bound of this damage to your random group.
+      Base Manacost: 3
+
+  Hellish creatures might have items that reduce or increase their
+    Fire resists, like armor that gives +10%, but helm that
+    reduces -15%.
+
+  Fire creatures might have increased resists agains fire.
+
+  Gates of Fire: A location that may consist of fire units (fire birds,
+    fire elementals, etc.). Once defeated, some fire units will be
+    suggested for hiring here (even if they weren't the part of guard).
+
+  IDEA: there might be locations that consume mana in order
+    to create units.
+*/
