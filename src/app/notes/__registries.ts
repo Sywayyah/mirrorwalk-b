3d64405b9ab5beeee1db60e077a41445b6c58a64
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2023-05-20 16:17:21 +0500

    Registries system note

---
+++ src/app/notes/__registries.ts
@@ -0,0 +1,6 @@
+/*
+  Entities might be better to get registered in some kind of registries
+  with ids. Referencing entities by ids might be a powerful tool.
+
+  IDEA: In theory, registries can be combined with current systems.
+*/
