8c29314ca7141d49c09c0eb4b5a911cae4773415
Yauheni Zharykau <Yauheni_Zharykau@epam.com>
2023-05-20 16:08:04 +0500

    Add base for level 7 Constellation units

--- src/app/core/fractions/constellation/fraction.ts
+++ src/app/core/fractions/constellation/fraction.ts
@@ -1,6 +1,6 @@
 import { Fractions, FractionsEnum } from '../fractions';
 
-export type CONSTELLATION_UNIT_TYPES = 'Sprite' | 'Fencer' | 'Sagittar' | 'Chariot';
+export type CONSTELLATION_UNIT_TYPES = 'Sprite' | 'Fencer' | 'Sagittar' | 'Chariot' | 'StarDragon' | 'NightWyvern';
 
 export const constellationFraction = Fractions.createFraction<CONSTELLATION_UNIT_TYPES>(
   FractionsEnum.Constellation,
--- src/app/core/unit-types/constellation/units.ts
+++ src/app/core/unit-types/constellation/units.ts
@@ -121,3 +121,79 @@ constellationFraction.defineUnitType('Chariot', {
     gold: 14,
   },
 });
+
+/* Star Dragon/Night Wyvern, potential level 7 unit type for Constellation */
+// Main feature: Dream Aura
+// Dream Aura: Some events around Night Wyverns become a part of their dream.
+//  Gives +10%/+15% to All Resists to units around. May also provide armor with aura.
+
+constellationFraction.defineUnitType('NightWyvern', {
+  name: 'Night Wyvern',
+  level: 7,
+  mainPortraitUrl: AssetsImages.Melee,
+
+  baseRequirements: {
+    gold: 1500,
+    gems: 2,
+    redCrystals: 1,
+  },
+
+  baseStats: {
+    damageInfo: {
+      minDamage: 47,
+      maxDamage: 64,
+    },
+    speed: 22,
+    defence: 21,
+    health: 245,
+    attackRating: 17,
+  },
+
+  defaultTurnsPerRound: 1,
+  minQuantityPerStack: 1,
+
+  neutralReward: {
+    experience: 180,
+    gold: 150,
+  },
+  upgraded: true,
+})
+
+constellationFraction.defineUnitType('StarDragon', {
+  name: 'Star Dragon',
+  level: 7,
+  mainPortraitUrl: AssetsImages.Melee,
+
+  baseRequirements: {
+    gold: 1000,
+    gems: 1,
+    redCrystals: 1,
+  },
+
+  upgradeDetails: {
+    target: constellationFraction.getUnitType('NightWyvern'),
+    upgradeCost: {
+      gold: 500,
+      gems: 1,
+    },
+  },
+
+  baseStats: {
+    damageInfo: {
+      minDamage: 40,
+      maxDamage: 60,
+    },
+    speed: 20,
+    defence: 18,
+    health: 175,
+    attackRating: 16,
+  },
+
+  defaultTurnsPerRound: 1,
+  minQuantityPerStack: 1,
+
+  neutralReward: {
+    experience: 180,
+    gold: 150,
+  },
+});
