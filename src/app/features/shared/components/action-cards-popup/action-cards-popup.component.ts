385ca147f6f74b5296aa117f2e48a00c041537aa
Yauheni Zharykau <fakeemail@gmail.com>
2023-10-02 18:07:10 +0500

    Display event feed when action card is used

--- src/app/features/shared/components/action-cards-popup/action-cards-popup.component.ts
+++ src/app/features/shared/components/action-cards-popup/action-cards-popup.component.ts
@@ -1,14 +1,17 @@
 import { Component, inject } from '@angular/core';
-import { ActionCardStack } from 'src/app/core/action-cards';
+import { ActionCard, ActionCardStack } from 'src/app/core/action-cards';
+import { MeditateActionCard } from 'src/app/core/action-cards/player-actions';
 import { RemoveActionPoints } from 'src/app/core/events';
 import { CommonUtils } from 'src/app/core/utils';
 import { MwPlayersService } from 'src/app/features/services';
+import { ApiProvider } from 'src/app/features/services/api-provider.service';
 import { State } from 'src/app/features/services/state.service';
+import { UiEventFeedService } from 'src/app/features/services/ui-event-feed.service';
 import { EventsService } from 'src/app/store';
 import { BasicPopup } from '../popup-container';
 import { VfxService } from '../vfx-layer';
-import { MeditateActionCard } from 'src/app/core/action-cards/player-actions';
-import { ApiProvider } from 'src/app/features/services/api-provider.service';
+
+const iconFn = ({ icon, iconColor, bgColor }: ActionCard) => `<i class='ra ra-${icon}' style="background: ${bgColor}; color: ${iconColor}"></i>`;
 
 @Component({
   selector: 'mw-action-cards-popup',
@@ -21,6 +24,7 @@ export class ActionCardsPopupComponent extends BasicPopup<{}> {
   private readonly events = inject(EventsService);
   private readonly vfx = inject(VfxService);
   private readonly apiProvider = inject(ApiProvider);
+  private readonly eventFeed = inject(UiEventFeedService);
 
   public cards = this.playersService.getCurrentPlayer().actionCards;
 
@@ -29,6 +33,7 @@ export class ActionCardsPopupComponent extends BasicPopup<{}> {
   public handleCardClick(cardStack: ActionCardStack): void {
     console.log(cardStack);
     const card = cardStack.card;
+    this.eventFeed.pushPlainMessage(`${iconFn(card)} ${card.title} action card is used.`);
 
     cardStack.count--;
 
