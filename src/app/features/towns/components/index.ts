282c70c5a0b21a4f5cf3afe6c593d37bafa91604
Yauheni Zharykau <whatever@gmail.com>
2024-04-28 17:17:08 +0500

    Adjust popups with new shared component

--- src/app/features/towns/components/index.ts
+++ src/app/features/towns/components/index.ts
@@ -1,5 +1,5 @@
 export * from './build-popup/build-popup.component';
-export * from './garrison-popup/garrison-popup/garrison-popup.component';
+export * from './garrison-popup/garrison-popup.component';
 export * from './hiring-popup/hiring-popup.component';
 export * from './town-view/town-view.component';
 
