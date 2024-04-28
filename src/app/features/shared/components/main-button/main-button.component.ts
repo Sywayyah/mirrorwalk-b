fe90bb0bf952aeddeb387bbe52c5a3cc7f6b41a9
Yauheni Zharykau <whatever@gmail.com>
2024-04-28 17:36:08 +0500

    Adjust more popups

--- src/app/features/shared/components/main-button/main-button.component.ts
+++ src/app/features/shared/components/main-button/main-button.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'button[mw-main-button]',
-  templateUrl: './main-button.component.html',
+  template: '<ng-content />',
   styleUrls: ['./main-button.component.scss']
 })
 export class MainButtonComponent {
--- src/app/features/shared/components/unit-slots-action-popup/unit-slots-action-popup.component.ts
+++ src/app/features/shared/components/unit-slots-action-popup/unit-slots-action-popup.component.ts
@@ -7,7 +7,6 @@ import { BasicPopup } from '../popup-container';
 @Component({
   selector: 'mw-unit-slots-action-popup',
   templateUrl: './unit-slots-action-popup.component.html',
-  styleUrl: './unit-slots-action-popup.component.scss'
 })
 export class UnitSlotsActionPopupComponent extends BasicPopup<{ sourceSlot: UnitGroupSlot, targetSlot: UnitGroupSlot, postAction: () => void }>{
   private readonly players = inject(MwPlayersService);
