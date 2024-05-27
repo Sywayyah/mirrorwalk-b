00e978b9c55ae676ec40716576949ee90dde87ae
Yauheni Zharykau <mephdecadence@gmail.com>
2024-05-27 15:57:17 +0500

    Transfer ValueBar component to signals

--- src/app/core/utils/dependencies.ts
+++ src/app/core/utils/dependencies.ts
@@ -1,5 +1,9 @@
-import { ElementRef, inject } from '@angular/core';
+import { ElementRef, Renderer2, inject } from '@angular/core';
 
 export function injectHostElem(): HTMLElement {
   return inject(ElementRef).nativeElement as HTMLElement;
+}
+
+export function injectRenderer(): Renderer2 {
+  return inject(Renderer2);
 }
