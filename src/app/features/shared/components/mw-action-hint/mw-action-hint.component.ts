ec128d7ac7acd5b1b8c30a972ff1249ccc060e5b
Yauheni Zharykau <whatever@gmail.com>
2025-04-29 01:20:46 +0500

    Add hint to Max button in town

--- src/app/features/shared/components/mw-action-hint/mw-action-hint.component.ts
+++ src/app/features/shared/components/mw-action-hint/mw-action-hint.component.ts
@@ -1,43 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { BehaviorSubject, combineLatest } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
 import { ActionHintModel, ActionHintTypeEnum } from 'src/app/core/ui';
-import { MwPlayersService } from 'src/app/features/services';
+import { injectHostElem } from 'src/app/core/utils';
 import { ActionHintService } from 'src/app/features/services/mw-action-hint.service';
-import { EventsService } from 'src/app/store';
 
 @Component({
-    selector: 'mw-action-hint',
-    templateUrl: './mw-action-hint.component.html',
-    styleUrls: ['./mw-action-hint.component.scss'],
-    standalone: false
+  selector: 'mw-action-hint',
+  templateUrl: './mw-action-hint.component.html',
+  styleUrls: ['./mw-action-hint.component.scss'],
+  standalone: false,
 })
 export class MwActionHintComponent {
-  public hintActionTypes: typeof ActionHintTypeEnum = ActionHintTypeEnum;
+  private readonly actionHintService = inject(ActionHintService);
+  private readonly hostElem = injectHostElem();
 
-  public readonly hint$: BehaviorSubject<ActionHintModel | null> = new BehaviorSubject<ActionHintModel | null>(null);
+  readonly hintActionTypes: typeof ActionHintTypeEnum = ActionHintTypeEnum;
 
-  constructor(
-    // public readonly mwBattleState: BattleStateService,
-    public readonly players: MwPlayersService,
-    public events: EventsService,
-    public readonly actionHintService: ActionHintService,
-  ) {
+  readonly hint$: BehaviorSubject<ActionHintModel | null> = new BehaviorSubject<ActionHintModel | null>(null);
+
+  constructor() {
     // need to revisit this logic. it was designed mostly for battleground.
-    combineLatest([
-      this.actionHintService.disableActionHint$,
-      this.actionHintService.hintMessage$,
-    ]).pipe(
-      map(([disabled, actionHint]) => {
-        if (disabled) {
-          return null;
-        }
+    combineLatest([this.actionHintService.disableActionHint$, this.actionHintService.hintMessage$])
+      .pipe(
+        map(([disabled, actionHint]) => {
+          if (disabled) {
+            return null;
+          }
 
-        return actionHint;
-      }),
-      tap(actionHint => this.hint$.next(actionHint)),
-      takeUntilDestroyed(),
-    ).subscribe();
+          return actionHint;
+        }),
+        tap((actionHint) => {
+          this.hostElem.classList.toggle('hidden', !actionHint);
+          this.hint$.next(actionHint);
+        }),
+        takeUntilDestroyed(),
+      )
+      .subscribe();
   }
 }
