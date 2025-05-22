64a53ef01d3f1ceea26fbd48c78512b8e76cea52
Yauheni Zharykau <whatever@gmail.com>
2025-05-23 00:27:19 +0500

    Basic scenario editor adjustments

--- src/app/features/scenario-editor-mode/components/scenario-editor-screen/scenario-editor-screen.component.ts
+++ src/app/features/scenario-editor-mode/components/scenario-editor-screen/scenario-editor-screen.component.ts
@@ -1,9 +1,24 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { EntitiesRegisty } from 'src/app/core/entities';
 import { GameOpenMainScreen } from 'src/app/core/events';
 import { ItemBaseType } from 'src/app/core/items';
 import { UnitBaseType } from 'src/app/core/unit-types';
+import { SignalArrUtils } from 'src/app/core/utils/signals';
 import { EventsService } from 'src/app/store';
+
+interface Scenario {
+  name: string;
+  locations: object[];
+  customEntites: object;
+  namedScripts: Record<string, ScenarioScript>;
+}
+
+let counter = 0;
+class ScenarioScript {
+  type = signal('SpellScript');
+  name = signal(`New_Script_${counter++}`);
+  code = signal('');
+}
 
 @Component({
   selector: 'mw-scenario-editor-screen',
@@ -13,6 +28,10 @@ import { EventsService } from 'src/app/store';
   styleUrl: './scenario-editor-screen.component.scss',
 })
 export class ScenarioEditorScreenComponent {
+  readonly scenarios = signal<Scenario[]>([
+    { name: 'Default Scenario', locations: [], customEntites: [], namedScripts: {} },
+  ]);
+
   private readonly events = inject(EventsService);
 
   readonly entitiesRegistries = EntitiesRegisty;
@@ -20,7 +39,20 @@ export class ScenarioEditorScreenComponent {
   readonly unitTypes = this.entitiesRegistries.getRegisteredEntitiesMap().get('#unit') as UnitBaseType[];
   readonly itemTypes = this.entitiesRegistries.getRegisteredEntitiesMap().get('#item') as ItemBaseType[];
 
+  readonly scripts = signal([] as ScenarioScript[]);
+
   readonly selectedUnitType = signal(null as string | null);
+  readonly selectedItemType = signal(null as string | null);
+  readonly selectedScenario = signal(null as string | null);
+  readonly selectedScript = signal(null as string | null);
+
+  readonly activeScript = computed(() => this.scripts().find((script) => script.name() === this.selectedScript()));
+
+  addNewScript(): void {
+    const newScript = new ScenarioScript();
+    this.scripts.update(SignalArrUtils.addItem(newScript));
+    this.selectedScript.set(newScript.name());
+  }
 
   goToMainScreen() {
     this.events.dispatch(GameOpenMainScreen());
