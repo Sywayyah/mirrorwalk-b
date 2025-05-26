7f740fe9ba80edc116911f41823d216270091b09
Yauheni Zharykau <whatever@gmail.com>
2025-05-26 21:53:13 +0500

    Extract types into a separate file

--- src/app/features/scenario-editor-mode/components/scenario-editor-screen/scenario-editor-screen.component.ts
+++ src/app/features/scenario-editor-mode/components/scenario-editor-screen/scenario-editor-screen.component.ts
@@ -5,7 +5,7 @@ import { EntitiesRegisty } from 'src/app/core/entities';
 import { GameOpenMainScreen, OpenNewGameScreen } from 'src/app/core/events';
 import { HeroBase } from 'src/app/core/heroes';
 import { ItemBaseType } from 'src/app/core/items';
-import { createSpell, SpellActivationType, SpellBaseType } from 'src/app/core/spells';
+import { createSpell, SpellBaseType } from 'src/app/core/spells';
 import { UnitBaseType } from 'src/app/core/unit-types';
 import { CommonUtils } from 'src/app/core/utils';
 import { SignalArrUtils } from 'src/app/core/utils/signals';
@@ -16,166 +16,21 @@ import { SharedModule } from 'src/app/features/shared/shared.module';
 import { EventsService } from 'src/app/store';
 import { PanelContainerComponent } from '../../../shared/components/editors-ui/panel-container/panel-container.component';
 import { PanelComponent } from '../../../shared/components/editors-ui/panel/panel.component';
+import {
+  ACTIVATION_TYPE_OPTIONS,
+  ActivationTypeOption,
+  CustomHeroDefinition,
+  CustomSpellDefinition,
+  CustomUnitDefinition,
+  EntityTabs,
+  SavedScenarioLocalStorageModel,
+  SavedScriptLocalStorageModel,
+  SavedSpellLocalStorageModel,
+  ScenarioScript,
+  SCRIPT_TYPE_OPTIONS,
+  ScriptTypeOption,
+} from '../../config/types';
 
-class Scenario {
-  readonly id = crypto.randomUUID();
-  readonly name = signal('');
-  readonly locations = signal([]);
-  readonly namedScripts = signal([]);
-}
-
-enum ScriptType {
-  TargetedSpell,
-  Trigger,
-  InstantSpell,
-}
-class ScenarioScript {
-  static counter = 0;
-  readonly id = `custom_script_${ScenarioScript.counter}`;
-  readonly name = signal(`New_Script_${ScenarioScript.counter}`);
-  readonly type = signal(SCRIPT_TYPE_OPTIONS[0]);
-  readonly code = signal('');
-
-  constructor(id?: string) {
-    ScenarioScript.counter++;
-    if (id) {
-      this.id = id;
-    }
-  }
-
-  static fromSaved(saved: SavedScriptLocalStorageModel): ScenarioScript {
-    const newScript = new ScenarioScript(saved.id);
-
-    newScript.code.set(saved.code);
-    newScript.name.set(saved.name);
-    newScript.type.set(SCRIPT_TYPE_OPTIONS.find((option) => option.value === saved.type)!);
-    return newScript;
-  }
-}
-
-let unitsCounter = 0;
-class CustomUnitDefinition {
-  readonly name = signal(`New_Unit_Type_${unitsCounter++}`);
-  readonly level = signal(1);
-  readonly health = signal(10);
-  readonly damage = signal(5);
-}
-
-class CustomHeroDefinition {
-  static counter = 0;
-  readonly id = `custom_hero_${CustomHeroDefinition.counter}`;
-  readonly assetUrl = signal('heroes/hero-melee.png');
-  readonly name = signal(`New_Hero_Type_${CustomHeroDefinition.counter}`);
-  readonly spells = signal<CustomSpellDefinition[]>([]);
-  readonly items = signal([]);
-  readonly army = signal([]);
-
-  constructor(id?: string) {
-    CustomHeroDefinition.counter++;
-    if (id) {
-      this.id = id;
-    }
-  }
-}
-
-class CustomSpellDefinition {
-  static counter = 0;
-  readonly id = `custom_spell_${CustomSpellDefinition.counter}`;
-  readonly name = signal(`New_Spell_Type_${CustomSpellDefinition.counter}`);
-  readonly activationType = signal(ACTIVATION_TYPE_OPTIONS[0]);
-  readonly icon = signal('book');
-  readonly connectedScript = signal<null | ScenarioScript>(null);
-
-  constructor(id?: string) {
-    CustomSpellDefinition.counter++;
-    if (id) {
-      this.id = id;
-    }
-  }
-
-  static fromSaved(saved: SavedSpellLocalStorageModel, scripts: ScenarioScript[]): CustomSpellDefinition {
-    const newSpell = new CustomSpellDefinition(saved.id);
-    newSpell.name.set(saved.name);
-    newSpell.icon.set(saved.icon);
-    newSpell.connectedScript.set(scripts.find((script) => script.id === saved.linkedScriptId) ?? null);
-    newSpell.activationType.set(ACTIVATION_TYPE_OPTIONS.find((option) => option.value === saved.activationType)!);
-
-    return newSpell;
-  }
-}
-
-enum EntityTabs {
-  UnitTypes = 'Units',
-  Items = 'Items',
-  Spells = 'Spells',
-  Heroes = 'Heroes',
-  Locations = 'Locations',
-  Factions = 'Factions',
-}
-interface SavedScriptLocalStorageModel {
-  id: string;
-  name: string;
-  code: string;
-  type: ScriptType;
-}
-
-type SavedSpellLocalStorageModel = {
-  id: string;
-  name: string;
-  icon: string;
-  activationType: SpellActivationType;
-  linkedScriptId?: string;
-};
-
-// scenarios persistance, export (entire scenario, individual aspects, like triggers, map generators, etc.)
-
-interface SavedScenarioLocalStorageModel {
-  id: string;
-  name: string;
-  locations: object[];
-  customSpells: SavedSpellLocalStorageModel[];
-  customEntities: object[];
-  customScripts: SavedScriptLocalStorageModel[];
-}
-
-interface Option<T> {
-  label: string;
-  value: T;
-}
-interface ActivationTypeOption extends Option<SpellActivationType> {
-  label: string;
-}
-
-interface ScriptTypeOption extends Option<ScriptType> {}
-
-const SCRIPT_TYPE_OPTIONS: ScriptTypeOption[] = [
-  { label: 'Targeted Spell Script', value: ScriptType.TargetedSpell },
-  { label: 'Instant Spell Script', value: ScriptType.InstantSpell },
-  { label: 'Trigger Spell', value: ScriptType.Trigger },
-];
-
-const ACTIVATION_TYPE_OPTIONS: ActivationTypeOption[] = [
-  {
-    label: 'Targetable',
-    value: SpellActivationType.Target,
-  },
-  {
-    label: 'Instant',
-    value: SpellActivationType.Instant,
-  },
-  {
-    label: 'Passive',
-    value: SpellActivationType.Passive,
-  },
-  {
-    label: 'Buff',
-    value: SpellActivationType.Buff,
-  },
-  {
-    label: 'Debuff',
-    value: SpellActivationType.Debuff,
-  },
-];
 @Component({
   selector: 'mw-scenario-editor-screen',
   standalone: true,
@@ -194,6 +49,8 @@ const ACTIVATION_TYPE_OPTIONS: ActivationTypeOption[] = [
   styleUrl: './scenario-editor-screen.component.scss',
 })
 export class ScenarioEditorScreenComponent {
+  // adjust scenarios - make them remember counters and store main content inside an object
+  // provide some metadata on root level
   readonly scenarios = signal<SavedScenarioLocalStorageModel[]>([]);
 
   private readonly events = inject(EventsService);
